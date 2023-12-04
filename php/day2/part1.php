ad292564c4d0b12d7ba92f10fad694349f193009
Aidan Ward <wardaidan98@gmail.com>
2023-12-04 02:46:45 +0000

    Day 2

---
+++ php/day2/part1.php
@@ -0,0 +1,54 @@
+<?php
+$data = file('./input_data');
+// $data = file('./sample_data');
+// $data = file('./extra_data');
+$result = 0;
+
+$bag = [
+    'red' => 12,
+    'green' => 13,
+    'blue' => 14,
+];
+
+for ($x = 0; $x < count($data); $x++) {
+    $datum = $data[$x];
+    $proc = substr($datum, 5); // string without 'Game ' at the start
+    $ID = substr($proc, 0, stripos($proc, ':')); // the game ID 
+    $proc = substr($proc, stripos($proc, ':') + 2); // the pur game results
+
+    $rounds = explode(';', $proc);
+    for ($y = 0; $y < count($rounds); $y++) {
+        $rounds[$y] = explode(',', $rounds[$y]);
+
+        $fail = false;
+        for ($z = 0; $z < count($rounds[$y]); $z++) {
+            $f = explode(' ', trim($rounds[$y][$z]));
+
+            $r = [];
+            $r[$f[1]] = $f[0];
+
+            if (array_key_exists('blue', $r) && $r['blue'] > $bag['blue']) {
+                $fail = true;
+            }
+            if (array_key_exists('red', $r) && $r['red'] > $bag['red']) {
+                $fail = true;
+            }
+            if (array_key_exists('green', $r) && $r['green'] > $bag['green']) {
+                $fail = true;
+            }
+
+            if ($fail) {
+                break;
+            }
+        }
+        if ($fail) {
+            break;
+        }
+    }
+
+    if (!$fail) {
+        $result += $ID;
+    }
+}
+
+print $result . PHP_EOL;
---
+++ php/day2/part2.php
@@ -0,0 +1,43 @@
+<?php
+$data = file('./input_data');
+// $data = file('./sample_data');
+$result = 0;
+
+for ($x = 0; $x < count($data); $x++) {
+    $datum = $data[$x];
+    $proc = substr($datum, 5); // string without 'Game ' at the start
+    $ID = substr($proc, 0, stripos($proc, ':')); // the game ID 
+    $proc = substr($proc, stripos($proc, ':') + 2); // the pur game results
+
+    $gameMax = [
+        'red' => 0,
+        'blue' => 0,
+        'green' => 0,
+    ];
+
+    $rounds = explode(';', $proc);
+    for ($y = 0; $y < count($rounds); $y++) {
+        $rounds[$y] = explode(',', $rounds[$y]);
+
+        for ($z = 0; $z < count($rounds[$y]); $z++) {
+            $f = explode(' ', trim($rounds[$y][$z]));
+
+            $r = [];
+            $r[$f[1]] = $f[0];
+
+            if (array_key_exists('blue', $r) && $gameMax['blue'] < $r['blue']) {
+                $gameMax['blue'] = $r['blue'];
+            }
+            if (array_key_exists('red', $r) && $gameMax['red'] < $r['red']) {
+                $gameMax['red'] = $r['red'];
+            }
+            if (array_key_exists('green', $r) && $gameMax['green'] < $r['green']) {
+                $gameMax['green'] = $r['green'];
+            }
+        }
+    }
+
+    $result += ($gameMax['blue'] * $gameMax['red'] * $gameMax['green']);
+}
+
+print $result . PHP_EOL;
