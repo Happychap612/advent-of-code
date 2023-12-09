57901c593a254572cd03efbf50d2018060c8c339
Aidan Ward <wardaidan98@gmail.com>
2023-12-09 05:18:37 +0000

    2023 Day 3 Part 2

--- 2023/php/day03/part2.php
+++ 2023/php/day03/part2.php
@@ -1,44 +1,78 @@
 <?php
 
-// $table = file('./input.data');
+$table = file('./input.data');
 // $table = file('./input2.data');
 // $table = file('./input3.data');
 // $table = file('./extra.data');
 // $table = file('./extra2.data');
-$table = file('./sample.data');
+// $table = file('./extra3.data');
+// $table = file('./sample.data');
 
 $result = 0;
 $height = count($table);
 $width = strlen($table[$height - 1]);
 
-// LOOP THROUGH ROWS AND FIND NUMBERS
 for ($y = 0; $y < count($table); $y++) {
-    $row = $table[$y];
-    preg_match_all('!\*!', $row, $matches, PREG_OFFSET_CAPTURE);
+    preg_match_all('/\*/', $table[$y], $matches, PREG_OFFSET_CAPTURE);
     $matches = $matches[0];
+    if (count($matches) == 0) {
+        continue;
+    }
+
+    $checkRows = [];
+    if ($y > 0) {
+        $checkRows[] = $table[$y - 1];
+    }
+    $checkRows[] = $table[$y];
+    if ($y < $height - 1) {
+        $checkRows[] = $table[$y + 1];
+    }
 
 
     for ($m = 0; $m < count($matches); $m++) {
         $match = $matches[$m][0];
         $pos = $matches[$m][1];
-        $min = $min = $pos === 0 ? $pos : $pos - 1;
+        $min = $pos === 0 ? $pos : $pos - 1;
+        $max = $pos === ($width - 1) ? $pos : $pos + 1;
 
-        $noOfMatches = 0;
-        $matched = [];
+        $partCountForGear = 0; // the count of part numbers we've found for this gear
+        $partsForGear = []; // the list of part numbers we've found for this gear
 
-        //ABOVE
-        if ($y > 0) {
-            $checkRow = $table[$y - 1];
-            $searchString = substr($checkRow, $min, 3);
-            preg_match_all('!\d!', $row, $no, PREG_OFFSET_CAPTURE);
+        for ($r = 0; $r < count($checkRows); $r++) {
+            $row = $checkRows[$r];
+            $slice = substr($row, $min, 3);
+            preg_match_all('/\d{1,3}/', $slice, $sm, PREG_OFFSET_CAPTURE);
+
+            if (count($sm[0]) > 0) {
+                for ($smi = 0; $smi < count($sm[0]); $smi++) {
+                    $smAP = $min + $sm[0][$smi][1]; //Slice Match Actual Pos
+                    $smAL = strlen($sm[0][$smi][0]); //Slice Match Actual Length
+
+                    $checkSlice = substr($row, $smAP - 1, $smAL);
+                    while (!preg_match('/\./', $checkSlice) && preg_match('/\d{1,3}/', $checkSlice) && $smAP !== 0) {
+                        $smAP--;
+                        $smAL++;
+                        $checkSlice = substr($row, $smAP - 1, $smAL); // setup next checkSlice
+                    }
+
+                    $checkSlice = substr($row, $smAP, $smAL + 1); // && ($smAP + $smAL) <= $max
+                    while (!preg_match('/\./', $checkSlice) && preg_match('/\d{1,3}/', $checkSlice && ($smAP + $smAL) !== $width)) {
+                        $smAL++;
+                        $checkSlice = substr($row, $smAP, $smAL + 1); // setup next checkSlice
+                    }
+
+                    $partCountForGear++;
+                    $partsForGear[] = substr($row, $smAP, $smAL);
+                }
+            }
         }
-        if ($continue) {
-            $matchNo++;
-            continue;
+
+        // checked all the rows for that gear
+        if ($partCountForGear == 2) {
+            // print "$partsForGear[0] x $partsForGear[1]\n";
+            $result += $partsForGear[0] * $partsForGear[1];
         }
+    }
 }
 
 print 'Result: ' . $result . PHP_EOL;
-print 'Count: ' . $matchNo . PHP_EOL;
-print 'Result: ' . $noResult . PHP_EOL;
-print 'Count: ' . $noMatchNo . PHP_EOL . PHP_EOL;
