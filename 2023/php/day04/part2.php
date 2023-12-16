fddfe70a558d6cf9cfb668d01e594fe7b6c47f94
Aidan Ward <wardaidan98@gmail.com>
2023-12-16 10:50:18 +0000

    2023 Day 4 Part 2

---
+++ 2023/php/day04/part2.php
@@ -0,0 +1,20 @@
+<?php
+$cards = file('./input.data');
+// $cards = file('./sample.data');
+
+$result = [];
+for ($i = 0; $i < count($cards); $i++) {
+    $result[] = 1;
+}
+for ($i = 0; $i < count($cards); $i++) {
+    $numbers = array_map(fn ($s): array => explode(' ', preg_replace('/\s+/', ' ', trim($s))), explode('|', substr($cards[$i], strpos($cards[$i], ":") + 1)));
+    $inter = count(array_intersect($numbers[0], $numbers[1]));
+    if ($inter > 0) {
+        $toDupe = array_slice($result, ($i + 1), $inter, true);
+        foreach ($toDupe as $k => $v) {
+            $result[$k] += 1 * $result[$i];
+        }
+    }
+}
+
+print "Result: " . array_sum($result) . "\n";
