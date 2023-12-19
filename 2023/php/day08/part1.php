53ae897589f4db9fd7a3d22c5d91e7e94bca9cf0
Aidan Ward <wardaidan98@gmail.com>
2023-12-19 12:23:52 +0000

    2023 Day 8 Part 1

---
+++ 2023/php/day08/part1.php
@@ -0,0 +1,25 @@
+<?php
+$mapData = file('./input.data');
+// $mapData = file('./sample1.data');
+// $mapData = file('./sample2.data');
+
+$directions = array_shift($mapData);
+array_shift($mapData);
+
+$map = [];
+foreach ($mapData as $mapDatum) {
+    preg_match_all('!([A-Z])\w+!', substr($mapDatum, 6), $inst);
+    $map[substr($mapDatum, 0, 3)] = [
+        "L" => $inst[0][0],
+        "R" => $inst[0][1],
+    ];
+}
+
+$currentPos = "AAA";
+$dirLen = strlen($directions);
+
+for ($i = 0; $currentPos !== "ZZZ"; $i++) {
+    $currentPos = $map[$currentPos][$directions[$i % ($dirLen - 1)]];
+}
+
+print "Result: $i\n";
